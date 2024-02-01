ebe2759d82067dde113d8df24895acc5afc2f8ae
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-02-01 21:38:21 +0300

    fix: scroll для safari

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       } else {
         setIsVisibleNavbar(true);
         setNavbarBg(true);
-        if (window.scrollY === 0) {
+        if (window.scrollY <= 5) {
           setIsVisibleNavbar(true);
           setNavbarBg(false);
         } else {
