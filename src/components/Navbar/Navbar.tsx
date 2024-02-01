66dde41421d0dfde9a3f6dcd17923de5bfa20740
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-02-01 21:45:54 +0300

    fix: scrollbar safari

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       } else {
         setIsVisibleNavbar(true);
         setNavbarBg(true);
-        if (window.scrollY <= 20) {
+        if (window.scrollY <= 80) {
           setIsVisibleNavbar(true);
           setNavbarBg(false);
         } else {
