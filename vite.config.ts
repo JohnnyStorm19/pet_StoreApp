41784ed2e808837448e61cac6bff0e4a8e49b585
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-02-03 17:30:54 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://johnnystorm19.github.io/pet_StoreApp/'
   base: 'https://pet-store-app-dun.vercel.app/'
 })
+
+  // base: 'https://johnnystorm19.github.io/pet_StoreApp/'
